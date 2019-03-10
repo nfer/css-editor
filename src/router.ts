3374f9fe1647cc741d7da9b1e95a044b1460de38
Nfer Zhuang <nfer.zhuang@gmail.com>
2019-03-10 10:02:05 +0800

    add editor page with iview

--- src/router.ts
+++ src/router.ts
@@ -21,5 +21,10 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
     },
+    {
+      path: '/editor',
+      name: 'editor',
+      component: () => import(/* webpackChunkName: "editor" */ './views/Editor.vue'),
+    },
   ],
 });
