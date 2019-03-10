a8586497fe370f310f04649bc0d74609139b6aee
Nfer Zhuang <nfer.zhuang@gmail.com>
2019-03-10 22:06:47 +0800

    clean codes

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import iView from 'iview';
 import App from './App.vue';
 import router from './router';
-import store from './store';
 
 import 'iview/dist/styles/iview.css';
 import '@/styles/index.less';
@@ -12,6 +11,5 @@ Vue.use(iView);
 
 new Vue({
   router,
-  store,
   render: h => h(App),
 }).$mount('#app');
