327bcd2c72e72dbad9e16d267b28c47c69c90bfe
Nfer Zhuang <nfer.zhuang@gmail.com>
2019-03-10 22:05:07 +0800

    clean codes

--- src/router.ts
+++ src/router.ts
@@ -10,8 +10,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      redirect: '/editor',
+    },
+    {
+      path: '/editor',
+      name: 'editor',
+      component: () => import(/* webpackChunkName: "editor" */ './views/Editor.vue'),
     },
     {
       path: '/about',
@@ -20,11 +24,6 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-    {
-      path: '/editor',
-      name: 'editor',
-      component: () => import(/* webpackChunkName: "editor" */ './views/Editor.vue'),
     },
   ],
 });
